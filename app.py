9e079b7306a8cf070b6bf60660fa1f6e5e8df86a
maryasad <maryasad76@yahoo.com>
2025-07-20 12:07:39 +0200

    fakemode

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 if "HF_SPACE_ID" in os.environ:
     os.environ["FAKE_MODE"] = "true"
 
-st.warning(os.getenv)
+st.warning(os.getenv("FAKE_MODE"))
 
 if os.getenv("FAKE_MODE") == "true":
     st.warning("🚧 Running in demo mode. Real API is disabled.")
